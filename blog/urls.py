5a0d71a7d253917c2358c60c8cd15341de965557
soham <shelarsoham450@gmail.com>
2026-02-11 13:08:33 +0530

    first

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostCreateView, PostDetailView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('add/', PostCreateView.as_view(), name='post_add'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.views import View
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
+
 
 class PostListView(View):
     def get(self, request):
         all_posts = Post.objects.all()
         context = {'all_posts': all_posts}
         return render(request, 'blog/post_list.html', context)
+
+
+class PostCreateView(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_form.html', {'form': form})
+
+    def post(self, request):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+        return render(request, 'blog/post_form.html', {'form': form})
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
